2207ddf69d3c6cfd2424f04c6ab654b93dfc7ea7
Isabel Lopez Murillo <isaana@email.unc.edu>
2025-03-18 16:35:04 -0400

    change to module

--- barber.ex
+++ barber.ex
@@ -1,5 +1,9 @@
-# Define the consts
-chairs = 6 
+defmodule barber do 
+    # Define the consts
+    chairs = 6 
+    free_seats = 6  
+    num_waiting = 0
 
 
-IO.puts("Starting the barbershop!")
+
+    IO.puts("Starting the barbershop!")
