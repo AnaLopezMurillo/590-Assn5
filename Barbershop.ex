51e63af9092e482deed67e958d67dc75bf8f4869
Isabel Lopez Murillo <isaana@email.unc.edu>
2025-03-23 14:15:45 -0400

    Changing file names

--- Barbershop.ex
+++ Barbershop.ex
@@ -1,75 +1,111 @@
 # team members: Isabel Lopez Murillo and Caitlyn Kim
 
 defmodule Barbershop do
-  def start do
-    IO.puts("Starting the barbershop!\n")
+    # starting the barbershop simulation, setting up the barber and receptionist
+    def start do
+        IO.puts("Starting the barbershop!")
 
-    # Spawn barber and receptionist processes
-    barber = spawn(fn -> barber_loop() end)
-    receptionist = spawn(fn -> receptionist_loop(barber, []) end)
+        barber = spawn(__MODULE__, :barber_process, [nil])
+        waiting_room = spawn(__MODULE__, :waiting_room_process, [barber, 6, []])
+        receptionist = spawn(__MODULE__, :receptionist_process, [waiting_room])
 
-    # Start generating customers
-    spawn(fn -> generate_customers(receptionist) end)
+        send(barber, {:set_waiting_room, waiting_room})
 
-    # Keep the main process alive
-    Process.sleep(:infinity)
-  end
-
-  defp barber_loop do
-    receive do
-      {:next_customer, customer} ->
-        IO.puts("Barber is cutting hair for customer #{customer}.")
-        :timer.sleep(:rand.uniform(3000))  # Simulating haircut (randomly betwen 0-3 seconds)
-        IO.puts("Customer #{customer} is done. Barber is available.\n")
-        barber_loop()
-      :no_one_waiting ->
-        IO.puts("No customers. Barber is sleeping...")
-        barber_loop()
+        customer_creator(receptionist, 1)
     end
-  end
 
-  defp receptionist_loop(barber, waiting_list) do
-    receive do
-      {:customer, id} ->
-        IO.puts("Customer #{id} arrives at the shop.")
+    # recursively creates new customers at random intervals
+    def customer_creator(receptionist, id) do
+        spawn(__MODULE__, :customer_process, [receptionist, id])
+        Process.sleep(:rand.uniform(5000))
+        customer_creator(receptionist, id + 1)
+    end
 
-        case length(waiting_list) do
-          0 ->
-            send(barber, {:next_customer, id})  # send immediately
-            # customer(receptionist_loop)
-            send(barber, {:no_one_waiting})     # tell barber he can sleep after this
-            receptionist_loop(barber, waiting_list)
-          n when n < 5 ->
-            IO.puts("Customer #{id} sits in the waiting area.")
-            new_list = waiting_list ++ [id]  # add this customer to the waiting list
-
-            [next | rest] = new_list
-            send(barber, {:next_customer, next})
-            receptionist_loop(barber, rest)
-          _ ->
-            # waiting list full
-            # send(customer, {:rejected, id})
-            receptionist_loop(barber, waiting_list)
+    # customer process that interacts with the receptionist to get a haircut
+    def customer_process(receptionist, id) do
+        IO.puts("Customer #{id} has arrived.")
+        send(receptionist, {:new_customer, self(), id})  # Send customer ID with self reference
+        receive do
+            :shop_full -> IO.puts("Customer #{id} left due to no available seats.")
+            :haircut_done -> IO.puts("Customer #{id} got a haircut and is leaving.")
         end
     end
-  end
 
-# customer process that interacts with receptionist
-defp customer(receptionist) do
-    send(receptionist, {:new_customer, self()})
-
-    receive do
-    :served -> IO.puts("Customer got a haircut and is leaving.")
-    :rejected -> IO.puts("Customer left due to no available seats.")
+    # receptionist handles the incoming customers and manages the seating queue
+    def receptionist_process(waiting_room) do
+        receive do
+            {:new_customer, customer, id} ->
+                IO.puts("Receptionist received new customer #{id}.")
+                send(waiting_room, {:seat_customer, customer, id})
+                receptionist_process(waiting_room)
+        end
     end
-end
 
-  defp generate_customers(receptionist) do
-    id = :rand.uniform(1000)
-    send(receptionist, {:customer, id})
-    :timer.sleep(:rand.uniform(3000))  # Customers arrive randomly
-    generate_customers(receptionist)  # loop
-  end
+    # waiting room manages the queue and interacts with the barber
+    def waiting_room_process(barber, max_size, queue) do
+        receive do
+            {:seat_customer, customer, id} ->
+                if length(queue) < max_size do
+                    new_queue = queue ++ [{customer, id}]
+                    IO.puts("Customer #{id} added to waiting room. Current queue size: #{length(new_queue)}")
+                    if length(queue) == 0 do
+                        send(barber, {:new_customer, customer, id})
+                    end
+                    waiting_room_process(barber, max_size, new_queue)
+                else
+                    IO.puts("No seats available. Customer #{id} turned away.")
+                    send(customer, :shop_full)
+                    waiting_room_process(barber, max_size, queue)
+            end
+
+            {:customer_done} ->
+                IO.puts("A customer is done. Removing from the queue.")
+                case queue do
+                    [{next_customer, next_id} | rest] ->
+                        send(barber, {:new_customer, next_customer, next_id})
+                        waiting_room_process(barber, max_size, rest)
+                    [] ->
+                        IO.puts("No more customers in the queue. Barber can rest.")
+                        send(barber, {:no_customers})  # Notify the barber that the queue is empty
+                        waiting_room_process(barber, max_size, [])
+                end
+
+            {:get_next_customer} ->
+                case queue do
+                    [{next_customer, next_id} | rest] ->
+                        IO.puts("Next customer #{next_id} is sent to the barber.")
+                        send(barber, {:new_customer, next_customer, next_id})
+                        waiting_room_process(barber, max_size, rest)
+                    [] ->
+                        IO.puts("No customers are in the waiting room.")
+                        send(barber, {:no_customers})  # Notify the barber that there are no customers
+                        waiting_room_process(barber, max_size, [])
+                end
+        end
+    end
+
+
+    # barber process handles the customer haircut and managing queue
+    def barber_process(waiting_room) do
+        receive do
+            {:set_waiting_room, room} ->
+                barber_process(room)
+
+            {:new_customer, customer, id} ->
+                IO.puts("Barber is cutting hair for customer #{id}.")
+                Process.sleep(:rand.uniform(3000))
+                IO.puts("Haircut finished for customer #{id}.")
+                send(customer, :haircut_done)
+                if waiting_room != nil do
+                    send(waiting_room, {:customer_done})
+                end
+                barber_process(waiting_room)
+
+            {:no_customers} ->
+                IO.puts("Barber is resting, no customers to serve.")
+                barber_process(waiting_room)
+        end
+    end
 end
 
 Barbershop.start()
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ By: Isabel Lopez Murillo and Caitlyn Kim
 
 ### Design Rationale
 
-We ended up running into several different issues like the barber just sleeping without checking if there are more customers in the queue, so from there we decided to explicitly design a waiting room. 
+We ended up running into several different issues like the barber just sleeping without checking if there are more customers in the queue, so from there we decided to explicitly design a waiting room. The waiting list works as a FIFO queue where customers' hair is cut based on priority of when they arrived. Once a customer is done, the waiting room process will send the next customer in the queue to the barber, who cuts their hair for a random time between 0-3 seconds. 
 
 We also had confusion when trying to track which customers went where, so we designed an id system where we know which customer is where through reporting which customer id is being served.
